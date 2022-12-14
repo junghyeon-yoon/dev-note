0e6afb2d4a0b13ea1ff53d2569fcfa5197a16837
junghyeon <junghyeon.yoon@hp.com>
2022-12-14 15:03:01 +0900

    Leetcode

---
+++ algo/leetcode/src/main/java/me/leetcode/solution/P35.java
@@ -0,0 +1,29 @@
+package me.leetcode.solution;
+
+public class P35 {
+
+    // https://leetcode.com/problems/search-insert-position/description/
+    public static void main(String[] args) {
+        var problem = new P35();
+        System.out.println(problem.searchInsert(new int[]{1,3,5,6}, 5));
+        System.out.println(problem.searchInsert(new int[]{1,3,5,6}, 2));
+        System.out.println(problem.searchInsert(new int[]{1,3,5,6}, 7));
+        System.out.println(problem.searchInsert(new int[]{1,3}, 0));
+    }
+
+    public int searchInsert(int[] nums, int target) {
+
+        int front = 0;
+        int back = nums.length-1;
+
+        while (front <= back) {
+            int mid =  (back + front) / 2;
+
+            if (target < nums[mid]) back = mid - 1;
+            else if (target > nums[mid]) front = mid + 1;
+            else return mid;
+        }
+
+        return front;
+    }
+}
---
+++ algo/leetcode/src/main/java/me/leetcode/solution/P9.java
@@ -0,0 +1,34 @@
+package me.leetcode.solution;
+
+public class P9 {
+    // https://leetcode.com/problems/palindrome-number/
+    public static void main(String[] args) {
+        var problem = new P9();
+        System.out.println(problem.isPalindrome(121));
+        System.out.println(problem.isPalindrome(-121));
+        System.out.println(problem.isPalindrome(10));
+        System.out.println(problem.isPalindrome(1));
+    }
+
+    public boolean isPalindrome(int x) {
+        if(x < 0) {
+            return false;
+        } else if(x < 10) {
+            return true;
+        } else {
+            String num = String.valueOf(x);
+            int front = 0;
+            int back = num.length()-1;
+            boolean result = true;
+
+            while(front < back) {
+                if(num.charAt(front++)!=num.charAt(back--)) {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
